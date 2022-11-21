a7aa623082196097a81b45c6000b8fa119a8eab0
ARC <you@example.com>
2022-11-21 07:49:04 -0800

    first commit

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -1,11 +1,17 @@
 const navBtn = document.querySelector("#nav-toggle");
 const links = document.querySelector(".nav-links");
 const navbar = document.querySelector(".navbar");
+const date = document.querySelector("#date")
 // add event listener
 
 navBtn.addEventListener("click", () => {
   links.classList.toggle("show-links");
 });
+
+const d = new Date();
+let year = d.getFullYear();
+
+date.textContent=year
 
 window.addEventListener("scroll", () => {
   let currentPo = window.scrollY;
